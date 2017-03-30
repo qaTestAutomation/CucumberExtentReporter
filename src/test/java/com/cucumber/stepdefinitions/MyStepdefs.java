1975e4764ce7d388d423868d2163c55df0065ffe
VimalRaj Selvam <email2vimalraj@gmail.com>
2017-03-30 08:58:45 +0530

    Fixed tests

--- src/test/java/com/cucumber/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/cucumber/stepdefinitions/MyStepdefs.java
@@ -1,12 +1,10 @@
 package com.cucumber.stepdefinitions;
 
 import com.cucumber.listener.Reporter;
-import com.sun.javaws.exceptions.InvalidArgumentException;
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
 
 import java.io.IOException;
 
@@ -16,28 +14,19 @@ public class MyStepdefs {
         throws IOException {
         Reporter.addStepLog("My test addStepLog message");
         Reporter.addScenarioLog("This is scenario log");
-        if (cukes == 1) {
-            throw new RuntimeException("Run time exception");
-        }
-        Assert.assertTrue(false);
         //        Reporter.addScreenCaptureFromPath(
         //            "/Users/vimalrajselvam/Downloads/best-resume-template-2016-3.jpg", "My title");
     }
 
     @Given("I have (\\d+) cukes in my bellies") public void I_have_cukes_in_my_bellies(int cukes) {
         System.out.format("Cukes: %n\n", cukes);
-        if (cukes == 7) {
-            throw new IllegalArgumentException("Super exception");
-        }
-        Assert.assertTrue(false);
     }
 
     @Then("^I print$") public void iPrint() throws Throwable {
         //        Assert.assertTrue(false);
-        Assert.assertTrue(false);
     }
 
     @When("^I login with credentials$") public void iLoginWithCredentials(DataTable table) {
-        Assert.assertTrue(false);
+
     }
 }
