d69de7c417fafbf7a0f542c09f5392a06c32e898
VimalRaj Selvam <email2vimalraj@gmail.com>
2017-06-25 18:29:34 +0530

    Added documentation to ExtentProperties

--- src/main/java/com/cucumber/listener/ExtentProperties.java
+++ src/main/java/com/cucumber/listener/ExtentProperties.java
@@ -17,26 +17,50 @@ public enum ExtentProperties {
         this.projectName = "default";
     }
 
+    /**
+     * Gets the report path
+     * @return The report path
+     */
     public String getReportPath() {
         return reportPath;
     }
 
+    /**
+     * Sets the report path
+     * @param reportPath The report path value
+     */
     public void setReportPath(String reportPath) {
         this.reportPath = reportPath;
     }
 
+    /**
+     * Gets the ExtentX server URL
+     * @return The ExtentX server URL
+     */
     public String getExtentXServerUrl() {
         return extentXServerUrl;
     }
 
+    /**
+     * Sets the ExtentX server URL
+     * @param extentXServerUrl The ExtentX server URL
+     */
     public void setExtentXServerUrl(String extentXServerUrl) {
         this.extentXServerUrl = extentXServerUrl;
     }
 
+    /**
+     * Gets the project name
+     * @return The project name
+     */
     public String getProjectName() {
         return projectName;
     }
 
+    /**
+     * Gets the project name
+     * @param projectName The project name
+     */
     public void setProjectName(String projectName) {
         this.projectName = projectName;
     }
